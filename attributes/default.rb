5b9022b7611a4e1aaa3a89868b3a4b27194a89f7
ilya <54044644+ipisman-costar@users.noreply.github.com>
2019-12-24 09:09:21 -0800

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,8 +20,8 @@
 #
 
 # git repository containing the ruby-build framework
-default['ruby_build']['git_url'] = 'https://github.com/rbenv/ruby-build.git'
-default['ruby_build']['git_ref'] = 'master'
+default['ruby_build']['git_url'] = 'https://github.com/CozyCo/ruby-build'
+#default['ruby_build']['git_ref'] = 'master'
 
 # default base path for a system-wide installed Ruby
 default['ruby_build']['default_ruby_base_path'] = '/usr/local/ruby'
